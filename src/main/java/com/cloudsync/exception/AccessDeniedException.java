6c02b9d50e168f2d3c41f61dc86e09b5adb735b6
AlgoArena2965 <subhanshgarg@gmail.com>
2026-02-25 04:56:49 +0530

    Refactor and improve codebase quality

---
+++ src/main/java/com/cloudsync/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.cloudsync.exception;
+
+public class AccessDeniedException extends CloudSyncException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/cloudsync/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.cloudsync.exception;
+
+public class AuthenticationException extends CloudSyncException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
